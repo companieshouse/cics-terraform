776a63bcd8c4d49aacc9286bcd370cb1665851b4
Jake Metherell <jake@fluffylogic.com>
2021-07-13 08:38:33 +0100

    Reduce ECR permissions down to most basic read access

--- groups/cics-infrastructure/iam.tf
+++ groups/cics-infrastructure/iam.tf
@@ -30,17 +30,9 @@ module "cics_profile" {
       resources = ["*"],
       actions = [
         "ecr:GetAuthorizationToken",
-        "ecr:BatchCheckLayerAvailability",
         "ecr:GetDownloadUrlForLayer",
-        "ecr:GetRepositoryPolicy",
-        "ecr:DescribeRepositories",
-        "ecr:ListImages",
-        "ecr:DescribeImages",
         "ecr:BatchGetImage",
-        "ecr:GetLifecyclePolicy",
-        "ecr:GetLifecyclePolicyPreview",
-        "ecr:ListTagsForResource",
-        "ecr:DescribeImageScanFindings"
+        "ecr:BatchCheckLayerAvailability"
       ]
     },
     {
