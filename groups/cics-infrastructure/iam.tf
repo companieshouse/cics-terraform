8c8d0008e655df77ffec1d3a2be4737d33d4011e
Jake Metherell <jake@fluffylogic.com>
2024-03-15 15:37:33 +0000

    Add CIS scanning policy to instance role

--- groups/cics-infrastructure/iam.tf
+++ groups/cics-infrastructure/iam.tf
@@ -59,3 +59,8 @@ module "cics_profile" {
     }
   ]
 }
+
+resource "aws_iam_role_policy_attachment" "inspector_cis_scanning_policy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonInspector2ManagedCisPolicy"
+  role       = module.cics_profile.aws_iam_role.name
+}
